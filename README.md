a28c974b1203f299835fe495d02f9afa043bec03
berkmahim <berkmahim620@gmail.com>
2021-10-25 03:23:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# js_film_poject
+ A film list project developed with vanilla javascript
---
+++ project.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("film-form");
+const titleForm = document.querySelector("#title");
+const directorForm = document.querySelector("#director");
+const urlForm = document.querySelector("#url");
+
+ui = new UI();
+eventlesteners();
+
+function eventlesteners(){
+    form.addEventListener("submit",addFilm);
+}
+
+function addFilm(e){
+    const title = titleForm.value;
+    const director = directorForm.value;
+    const url = urlForm.value
+
+    const newFilm = new Film(title,director,url);
+    ui.addFilmToUI(newFilm);
+    e.preventDefault();
+}
